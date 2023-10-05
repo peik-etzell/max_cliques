cf452e43bad9f4fa8b3eaa158dc686eba27fc970
Peik Etzell <peik.etzell@gmail.com>
2023-10-05 20:55:31 +0300

    Change to undirected graph as it should be

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,13 @@
 #include <boost/graph/adjacency_list.hpp>
 #include <boost/graph/bron_kerbosch_all_cliques.hpp>
 #include <boost/graph/erdos_renyi_generator.hpp>
+#include <boost/graph/undirected_graph.hpp>
 #include <boost/random/linear_congruential.hpp>
 #include <iostream>
 
 // https://www.boost.org/doc/libs/1_83_0/libs/graph/doc/erdos_renyi_generator.html
 // ^ Example constructing ER graph
-typedef boost::adjacency_list<> Graph;
+typedef boost::undirected_graph<> Graph;
 typedef boost::erdos_renyi_iterator<boost::minstd_rand, Graph> ERGen;
 
 static const float p = 0.5;
