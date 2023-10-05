c5a320fab9906224fd8b22379de68e8257f4672d
Peik Etzell <peik.etzell@gmail.com>
2023-10-05 21:07:59 +0300

    Log time taken, fix theoretical approximation

--- plot.py
+++ plot.py
@@ -1,26 +1,40 @@
-import math
+from math import log2
 import numpy as np
 import matplotlib.pyplot as plt
 
+fig, clique_axis = plt.subplots(1)
+time_axis = clique_axis.twinx()
+
 data = np.genfromtxt(fname='out.tsv', delimiter='\t')
 data = np.sort(data, axis=0)
-x, y = zip(*data)
-plt.plot(x, y, label='Real data')
-plt.legend()
+x, y_real, time = zip(*data)
 
 
-def k_theoretical(n: float):
-    return 2 * math.log2(n)
+def plot_real_data():
+    clique_axis.plot(x, y_real, '.', label='Real data')
+    fig.legend()
 
 
-k_v = np.vectorize(k_theoretical)
-x_prime = np.linspace(2, max(x), 10000)
-y_prime = k_v(x_prime)
-plt.plot(x_prime, y_prime, label='Theoretical')
-plt.legend()
+def plot_theoretical():
+    k_v = np.vectorize(lambda x: int(
+        2 * log2(x) - 2*log2(log2(x))))
+    y_theory = k_v(x)
+    clique_axis.plot(x, y_theory, label='2log(n) - 2loglog(n)')
+    fig.legend()
 
-plt.xlabel('Number of vertices')
-plt.ylabel('Size of maximum clique')
+
+def plot_time():
+    time_axis.plot(x, time, label='Time taken')
+    fig.legend()
+
+
+plot_real_data()
+plot_theoretical()
+plot_time()
+
+clique_axis.set_xlabel('Number of vertices')
+clique_axis.set_ylabel('Size of maximum clique')
+time_axis.set_ylabel('Time taken (s)')
 
 plt.savefig('plot.png')
 plt.show()
--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,10 @@
 #include <boost/graph/erdos_renyi_generator.hpp>
 #include <boost/graph/undirected_graph.hpp>
 #include <boost/random/linear_congruential.hpp>
+#include <chrono>
 #include <iostream>
+
+using namespace std::chrono;
 
 // https://www.boost.org/doc/libs/1_83_0/libs/graph/doc/erdos_renyi_generator.html
 // ^ Example constructing ER graph
@@ -16,11 +19,15 @@ int main(int argc, char** argv) {
     boost::minstd_rand gen;
 
 #pragma omp parallel for schedule(dynamic, 1)
-    for (int n = 2; n < 500; ++n) {
+    for (int n = 2; n < 1000; ++n) {
+        auto start = system_clock::now();
         Graph G(ERGen(gen, n, p), ERGen(), n);
         int omega = boost::bron_kerbosch_clique_number(G);
+        auto duration =
+            duration_cast<milliseconds>(system_clock::now() - start).count() *
+            0.001;
 #pragma omp critical
-        { std::cout << n << '\t' << omega << std::endl; }
+        { std::cout << n << '\t' << omega << '\t' << duration << std::endl; }
     }
 
     return 0;
